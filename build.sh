d0536c38c43fda225e6f2eecc60bd99c089b93cd
Seb Ospina <kraige@gmail.com>
2018-07-28 09:32:55 +0000

    Trying skaffold for pushing the image

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ if [[ -z $APP_NAME ]] || [[ -z $RUST_VERSION ]]; then
 fi
 docker pull $APP_NAME:$RUST_VERSION 2> /dev/null
 docker build -t $APP_NAME:$RUST_VERSION .
-docker push $APP_NAME:$RUST_VERSION
