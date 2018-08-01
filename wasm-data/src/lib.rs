1d36690a3746d68e6c568862402004f3d2dbd57b
Seb Ospina <kraige@gmail.com>
2018-08-01 20:27:08 +0000

    Adding wasm-bindgen dependency

--- wasm-data/src/lib.rs
+++ wasm-data/src/lib.rs
@@ -1,3 +1,16 @@
+#![feature(use_extern_macros)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
 #[no_mangle]
 pub extern fn add_one(a: u32) -> u32 {
   a + 1
@@ -6,7 +19,7 @@ pub extern fn add_one(a: u32) -> u32 {
 mod tests {
     use super::*;
     #[test]
-    fn it_works() {
+    fn it_adds_one() {
         assert_eq!(add_one(1), 2);
     }
 }
