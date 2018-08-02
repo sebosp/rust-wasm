8c04eab06c9899e335d554ad7324dcc4ac3eeeeb
Seb Ospina <kraige@gmail.com>
2018-08-02 19:03:46 +0000

    Adding game_of_life index calc

--- wasm-data/src/lib.rs
+++ wasm-data/src/lib.rs
@@ -2,24 +2,52 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+  Dead = 0,
+  Alive = 1,
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub struct Universe {
+  width: u32,
+  height: u32,
+  cells: Vec<Cell>,
 }
+
+impl Universe {
+  fn get_index(&self, row: u32, column: u32) -> usize {
+    (row * self.width + column) as usize
+  }
+}
+
 #[no_mangle]
 pub extern fn add_one(a: u32) -> u32 {
   a + 1
 }
 #[cfg(test)]
 mod tests {
-    use super::*;
-    #[test]
-    fn it_adds_one() {
-        assert_eq!(add_one(1), 2);
-    }
+  use super::*;
+  #[test]
+  fn it_adds_one() {
+    assert_eq!(add_one(1), 2);
+  }
+  #[test]
+  fn it_gets_index() {
+    let uni = Universe {
+      width: 4,
+      height: 4,
+      cells: vec![
+        Cell::Dead,Cell::Dead,Cell::Dead,Cell::Dead,
+        Cell::Dead,Cell::Dead,Cell::Dead,Cell::Dead,
+        Cell::Dead,Cell::Dead,Cell::Dead,Cell::Dead,
+        Cell::Dead,Cell::Dead,Cell::Dead,Cell::Dead,
+      ],
+    };
+    assert_eq!(uni.get_index(0,0), 0);
+    assert_eq!(uni.get_index(1,2), 6);
+    assert_eq!(uni.get_index(2,2), 10);
+    assert_eq!(uni.get_index(3,3), 15);
+  }
 }
